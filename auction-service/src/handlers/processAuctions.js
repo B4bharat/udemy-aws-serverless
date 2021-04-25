559fc620c528cfbbc83dff74b344de488c74ed4f
Bharat Poptwani <bharat.p@goodworker.in>
2021-04-26 01:21:04 +0530

    Creating a Global Secondary Index (status, endingAt)

---
+++ auction-service/src/handlers/processAuctions.js
@@ -0,0 +1,6 @@
+// Creating a Global Secondary Index (status, endingAt)
+async function processAuctions(event, context) {
+  console.log('processAuctions');
+}
+
+export const handler = processAuctions;
